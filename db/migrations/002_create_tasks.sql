427d7bf27048b9f9d13e337e2ad7ac552649a351
Makram Kamaleddine <makram.kd@gmail.com>
2021-08-07 11:38:06 -0400

    Remove unnecessary tasks table
    Add index to speed up order by queries

--- db/migrations/002_create_tasks.sql
+++ db/migrations/002_create_tasks.sql
@@ -1,18 +1,12 @@
 BEGIN;
 
-CREATE TABLE tasks(
-    id UUID PRIMARY KEY,
-    command TEXT NOT NULL,
-    frequency TEXT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    servers TEXT[] NOT NULL
-);
-
 CREATE TABLE task_outputs(
     task_id UUID NOT NULL,
     outputs JSONB NOT NULL,
     completed_at TIMESTAMPTZ
 );
+
+CREATE INDEX task_outputs_completed_at_idx ON task_outputs (completed_at);
 
 INSERT INTO schema_migrations (version) VALUES ('002_create_tasks');
 
