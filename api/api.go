ea6aad79e22c78f5f917a036256257c4659c9911
Makram Kamaleddine <makram.kd@gmail.com>
2021-08-06 20:23:13 -0400

    Move API request/response objects to common module

---
+++ api/api.go
@@ -0,0 +1,28 @@
+package api
+
+// ---- Server API ---
+
+// CreateTaskRequest is used to create a task from the server API.
+type CreateTaskRequest struct {
+	Command   string `json:"command"`
+	Frequency string `json:"frequency"`
+}
+
+type CreateTaskResponse struct {
+	TaskID string `json:"task_id"`
+}
+
+type UpdateTaskRequest struct {
+	AgentID string `json:"agent_id"`
+	Stdout  string `json:"stdout"`
+	Stderr  string `json:"stderr"`
+}
+
+// ---- Agent API ----
+
+// ScheduleTaskRequest is used to schedule a task from the agent API.
+type ScheduleTaskRequest struct {
+	TaskID    string `json:"task_id"`
+	Command   string `json:"command"`
+	Frequency string `json:"frequency"`
+}
